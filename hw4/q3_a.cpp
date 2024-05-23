74992ac231b692b98617e97c62580a1c48c58357
Mike <mikehu980@gmail.com>
2024-05-24 07:01:57 +0800

    working on code4 q3a

--- hw4/q3_a.cpp
+++ hw4/q3_a.cpp
@@ -12,3 +12,82 @@ public:
     virtual void Push(const T&) = 0;
     virtual void Pop() = 0;
 };
+
+
+template <class T>
+class MaxHeap : public class MaxPQ<T>
+{
+    template <class U>
+    friend void ChangeSize1D(U* &a, const int oldSize, const int newSize);
+public:
+    MaxHeap(int theCapacity = 10); // constructor
+    bool IsEmpty() const; //return true iff empty
+    const T& Top() const; //return reference to the max
+    void Push(const T&);
+    void Pop();
+private:
+    T* heap; // element array
+    int heapSize; // number of elements in heap
+    int capacity; // size of the element array
+};
+
+template <class T>
+MaxHeap<T>::MaxHeap(int theCapacity) // constructor
+{
+    if(theCapacity < 1) throw "Capacity must be >= 1";
+    capacity = theCapacity;
+    heapSize = 0;
+    heap = new T [capacity + 1]; // heap[0] unused
+}
+
+template <class T>
+void MaxHeap<T>::Push(const T& e)
+{ // add element e to max heap
+    if (heapSize == capacity) { // double the capacity
+        ChangeSize1D(heap, capacity, 2 * capacity);
+        capacity *= 2;
+    }
+    int currentNode = ++heapSize;
+    while (currentNode != 1 && heap[currentNode / 2] < e) { // bubbling up
+        heap[currentNode] = heap[currentNode / 2];//move parent down
+        currentNode /= 2;
+    }
+    heap[currentNode] = e;
+}
+
+template <class T>
+void MaxHeap<T>::Pop()
+{ // Delete max element
+    if (IsEmpty ()) throw "Heap is empty. Cannot delete.";
+    heap[1].~T(); // delete max element
+    T lastE = heap[heapSize--]; // remove last element from heap
+    // trickle down to find a position to place the last element
+    int currentNode = 1; // root
+    int child = 2; // a child of current node
+    while (child <= heapSize) {
+        // set child to larger child of currentNode
+        if (child < heapSize && heap[child] < heap[child + 1]) child++;
+        if (lastE >= heap[child]) break;
+        heap[currentNode] = heap[child]; // move child up
+        currentNode = child; child *= 2; // move down a level
+    }
+    heap[currentNode] = lastE;
+}
+
+template <class U>
+void ChangeSize1D(U* &a, const int oldSize, const int newSize)
+{
+    if (newSize < 0) throw "New length must be >= 0";
+    U *temp = new U[newSize];
+    int number = min(oldSize, newSize);
+    copy(a, a + number, temp);
+    delete []a;
+    a = temp;
+}
+
+
+int main()
+{
+    
+    return 0;
+}
